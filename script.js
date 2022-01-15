8fcd6a606af4b034c614b7d3e465dcb29c5b27e6
Lane Melancon <melanconlane@gmail.com>
2022-01-15 10:13:09 -0700

    Updated index.html to locate script.js

--- script.js
+++ script.js
@@ -25,25 +25,3 @@ btnHamburger.addEventListener("click", function () {
 		});
 	}
 });
-
-btnHamburger.addEventListener("touchstart", function () {
-	console.log("click hamburger");
-
-	if (header.classList.contains("open")) {
-		// Close Hamburger Menu
-		body.classList.remove("noscroll");
-		header.classList.remove("open");
-		fadeElems.forEach(function (element) {
-			element.classList.remove("fade-in");
-			element.classList.add("fade-out");
-		});
-	} else {
-		// Open Hamburger Menu
-		body.classList.add("noscroll");
-		header.classList.add("open");
-		fadeElems.forEach(function (element) {
-			element.classList.remove("fade-out");
-			element.classList.add("fade-in");
-		});
-	}
-});
